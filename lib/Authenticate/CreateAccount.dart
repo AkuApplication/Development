a50599b6011743334d0204c6ca29411a6c061993
Hafizzan Kadir <19FTT1159@student.pb.edu.bn>
2021-09-02 16:27:55 +0800

    Change to Aku Application Firebase Account

--- lib/Authenticate/CreateAccount.dart
+++ lib/Authenticate/CreateAccount.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/Authenticate/Methods.dart';
+import 'package:chat_app/Screens/homepage.dart';
 import 'package:flutter/material.dart';
 
 import '../Screens/Counsultation/HomeScreen.dart';
@@ -128,7 +129,7 @@ class _CreateAccountState extends State<CreateAccount> {
                 isLoading = false;
               });
               Navigator.push(
-                  context, MaterialPageRoute(builder: (_) => HomeScreen()));
+                  context, MaterialPageRoute(builder: (_) => HomePage()));
               print("Account Created Sucessfull");
             } else {
               print("Login Failed");
